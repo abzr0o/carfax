760761b272a0ffb989cf2a7c38c975eb6f540e25
abzro0o <adqe98877@gmail.com>
2023-03-14 20:32:42 +0300

    ok

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -ex
-curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - && sudo apt-get install -y nodejs
-# get source code from githubt
-git clone https://github.com/abzr0o/carfax.git
-#get in project dir
-cd carfax
-#give permission
-sudo chmod -R 755 .
-#install node module
+
 sudo npm i -g yarn
 sudo rm packge-lock.json
 sudo yarn install
@@ -19,4 +12,4 @@ sudo apt-get update && sudo apt-get install curl gnupg -y \
     && sudo apt-get install google-chrome-stable -y --no-install-recommends \
     && sudo rm -rf /var/lib/apt/lists/*
 # start the app
-sudo node dist/index.js
+sudo node dist/index.js > /dev/null 2>&1 &
